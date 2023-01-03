a2c6df39db745026d2c8acf4d22cdb696b440768
Shivahiren <shivahiren108@gmail.com>
2023-01-03 22:01:10 +0000

    Initial commit

---
+++ src/main/java/com/restful/booker/constants/EndPoint.java
@@ -0,0 +1,18 @@
+package com.restful.booker.constants;
+
+
+public class EndPoint {
+
+    /**
+     * This is Endpoints of student api
+     */
+    public static final String GET_BOOKING = "/{id}";
+    public static final String GET_BOOKING_ID = "/{id}";
+    public static final String UPDATE_BOOKING_BY_ID = "/{id}";
+    public static final String DELETE_BOOKING_BY_ID = "/{id}";
+
+    /**
+     * This is Endpoints of Authentication api
+     */
+
+}
